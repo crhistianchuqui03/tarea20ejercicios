40c6e097bed1f877c424c84d9e6e9a42069d77c2
crhistianchuqui02 <crhistian.chuquitarqui@upeu.edu.pe>
2022-07-22 02:17:46 -0500

    matrices

---
+++ tarea20ejerciciosCCH/ejercicios20Matrices/ejerciciosCCH.java
@@ -0,0 +1,483 @@
+package ejercicios20Matrices;
+
+import java.util.Scanner;
+
+public class ejerciciosCCH {
+    Scanner lt = new Scanner(System.in);
+
+    public void ejercicio05cch() {
+        System.out.println("Ingrese la dimesion de Matriz:");
+        int dimension =lt.nextInt();
+        System.out.println("Ingrese el valor inicial:");
+        int valor =lt.nextInt();
+        String[][] matriz = new String[dimension][dimension];
+        for (int f = 0; f < matriz.length; f++) {
+            for (int c = 0; c <= f; c++) {
+                matriz[f][(dimension - 1) - c] = String.valueOf(valor);
+                valor++;
+            }
+        }
+        imprimirMatriz(matriz);
+    }
+    public void ejercicio06cch() {
+        System.out.println("Ingrese dimesion de Matriz:");
+        int dimension = lt.nextInt();
+        System.out.println("Ingrese valor inicial:");
+        int valor = lt.nextInt();
+        String[][] matriz = new String[dimension][dimension];
+        for (int f = 0; f < matriz.length; f++) {
+            for (int c = 0; c <= f; c++) {
+                matriz[f][c] = String.valueOf(valor);
+                valor++;
+            }
+        }
+        imprimirMatriz(matriz);
+    }
+
+    public void ejercicio07cch() {
+        String[][] matriz;
+        System.out.println("Ingrese dimesion de Matriz:");
+        int dimension = lt.nextInt();
+        System.out.println("Ingrese valor inicial:");
+        int valor = lt.nextInt();
+        matriz = new String[dimension][dimension];
+        for (int f = 0; f < matriz.length; f++) {
+            for (int c = 0; c <= (matriz.length - 1) - f; c++) {
+                matriz[f][c] = String.valueOf(valor);
+                valor++;
+            }
+        }
+        imprimirMatriz(matriz);
+    }
+
+    public void ejercicio08cch() {
+        String[][] matriz;
+        System.out.println("Ingrese dimesion de Matriz:");
+        int dimension = lt.nextInt();
+        System.out.println("Ingrese valor inicial:");
+        int valor = lt.nextInt();
+        matriz = new String[dimension][dimension];
+        for (int f = 0; f < matriz.length; f++) {
+            for (int c = f; c < matriz[f].length; c++) {
+                matriz[f][c] = String.valueOf(valor); 
+                valor++;        
+            }          
+        }
+        imprimirMatriz(matriz);
+    }
+    
+    public void ejercicio09cch() {
+        System.out.println("Ingrese dimesion de Matriz:");
+        int dimension = lt.nextInt();
+        System.out.println("Ingrese valor inicial:");
+        int valor = lt.nextInt();
+        String[][] matriz = new String[dimension][dimension];
+        for (int f = 0; f < matriz.length; f++) {
+            for (int c = (matriz[0].length - 1) - f; c <= matriz[0].length - 1; c++) {
+                matriz[f][c] = String.valueOf(valor);
+                valor++;
+            }
+        }
+        imprimirMatriz(matriz);
+    }
+
+    public void ejercicio10cch() {
+        System.out.println("Ingrese dimesion de Matriz:");
+        int dimension = lt.nextInt();
+        System.out.println("Ingrese valor inicial:");
+        int valor = lt.nextInt();
+        String[][] matriz = new String[dimension][dimension];
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j <= i; j++) {
+                matriz[i][i - j] = String.valueOf(valor);
+                valor++;
+            }
+        }
+        imprimirMatriz(matriz);
+    }
+
+    public void ejercicio11cch() {
+        String[][] matriz;
+        System.out.println("Ingrese dimesion de Matriz:");
+        int dimension = lt.nextInt();
+        System.out.println("Ingrese valor inicial:");
+        int valor = lt.nextInt();
+        matriz = new String[dimension][dimension];
+        for (int f = 0; f < matriz.length; f++) {
+            for (int c = (matriz.length - 1) - f; c >= 0; c--) {
+                matriz[f][c] = String.valueOf(valor);
+                valor++;
+            }
+        }
+        imprimirMatriz(matriz);
+    }
+
+    public void ejercicio12cch() {
+        String[][] matriz;
+        System.out.println("Ingrese dimesion de Matriz:");
+        int dimension = lt.nextInt();
+        System.out.println("Ingrese valor inicial:");
+        int valor = lt.nextInt();
+        matriz = new String[dimension][dimension];
+        for (int f = 0; f < matriz.length; f++) {
+            for (int c = matriz[f].length-1; c >= f; c--) {
+                matriz[f][c] = String.valueOf(valor); 
+                valor++;        
+            }          
+        }
+        imprimirMatriz(matriz);
+    }
+
+    public void ejercicio13cch() {
+        String[][] matriz;
+        System.out.println("Ingrese dimesion de Matriz:");
+        int dimension = lt.nextInt();
+        System.out.println("Ingrese valor inicial:");
+        int valor = lt.nextInt();
+        matriz = new String[dimension][dimension];
+        for (int c = 0; c < matriz.length; c++) {
+            for (int f = 0; f <= (matriz.length - 1) - c; f++) {
+                matriz[f][c] = String.valueOf(valor);
+                valor++;
+            }
+        }
+        imprimirMatriz(matriz);
+    }
+
+    public void ejercicio19cch() {
+        String[][] matriz;
+        System.out.println("Ingrese la dimesion de la matriz:");
+        int dimension = lt.nextInt();
+        System.out.println("Ingrese el valor inicial");
+        int valor = 0;
+        matriz = new String[dimension][dimension];
+        for (int fila = 0; fila >= 0; fila--) {
+            for (int columna = matriz[fila].length-1; columna >= fila; columna--) {
+                matriz[columna][fila] = String.valueOf(valor); 
+                valor=valor+1;        
+            }          
+        }
+        imprimirMatriz(matriz);
+    }
+
+    public void ejercicio20cch() {
+        String[][] matriz;
+        System.out.println("Ingrese dimesion de Matriz:");
+        int dimension = lt.nextInt();
+        System.out.println("Ingrese valor inicial:");
+        int valor = lt.nextInt();
+        matriz = new String[dimension][dimension];
+        for (int f = (matriz[0].length - 1); f >= 0; f--) {
+            for (int c = matriz[f].length-1; c >= f; c--) {
+                matriz[c][f] = String.valueOf(valor); 
+                valor++;        
+            }          
+        }
+        imprimirMatriz(matriz);
+    }
+
+    public void ejercicio22cch() {
+        String[][] matriz;
+        System.out.println("Ingrese dimesion de Matriz:");
+        int dimension = lt.nextInt();
+        System.out.println("Ingrese valor inicial:");
+        int valor = lt.nextInt();
+        matriz = new String[dimension][dimension];
+        for (int c = 0; c < matriz[0].length; c++) {
+            if (c % 2 == 0) {
+                for (int f = matriz.length - 1; f >= 0; f--) {
+                    matriz[f][c] = String.valueOf(valor);
+                    valor = valor + 1;
+                }
+            } else {
+                for (int f = 0; f < matriz.length; f++) {
+                    matriz[f][c] = String.valueOf(valor);
+                    valor = valor + 1;
+                }
+            }
+
+        }
+        imprimirMatriz(matriz);
+    }
+
+    public void ejercicio23cch() {
+        String[][] matriz;
+        System.out.println("Ingrese dimesion de Matriz:");
+        int dimension = lt.nextInt();
+        System.out.println("Ingrese valor inicial:");
+        int valor = lt.nextInt();
+        matriz = new String[dimension][dimension];
+        for (int c = (matriz[0].length - 1); c >= 0; c--) {
+            if (c % 2 == 0) {
+                for (int f = 0; f < matriz.length; f++) {
+                    matriz[f][c] = String.valueOf(valor);
+                    valor = valor + 1;
+                }
+            } else {
+                for (int f = matriz.length - 1; f >= 0; f--) {
+                    matriz[f][c] = String.valueOf(valor);
+                    valor = valor + 1;
+                }
+            }
+
+        }
+        imprimirMatriz(matriz);
+    }
+
+    public void ejercicio24cch() {
+        String[][] matriz;
+        System.out.println("Ingrese dimesion de Matriz:");
+        int dimension = lt.nextInt();
+        System.out.println("Ingrese valor inicial:");
+        int valor = lt.nextInt();
+        matriz = new String[dimension][dimension];
+        for (int c = (matriz[0].length - 1); c >= 0; c--) {
+            if (c % 2 == 0) {
+                for (int f = matriz.length - 1; f >= 0; f--) {
+                    matriz[f][c] = String.valueOf(valor);
+                    valor = valor + 1;
+                }
+            } else {
+                for (int f = 0; f < matriz.length; f++) {
+                    matriz[f][c] = String.valueOf(valor);
+                    valor = valor + 1;
+                }
+            }
+
+        }
+        imprimirMatriz(matriz);
+    }
+
+    public void ejercicio25cch() {
+        String[][] matriz;
+        System.out.println("Ingrese dimesion de Matriz:");
+        int dimension = lt.nextInt();
+        System.out.println("Ingrese valor inicial:");
+        int valor = lt.nextInt();
+        matriz = new String[dimension][dimension];
+        for (int f = 0; f < matriz.length; f++) {
+            if (f % 2 == 0) {
+                for (int c = 0; c < matriz.length; c++) {
+                    matriz[f][c] = String.valueOf(valor);
+                    valor = valor + 1;
+                }
+            } else {
+                for (int c = matriz.length - 1; c >= 0; c--) {
+                    matriz[f][c] = String.valueOf(valor);
+                    valor = valor + 1;
+                }
+            }
+
+        }
+        imprimirMatriz(matriz);
+    }
+
+    public void ejercicio26cch() {
+        String[][] matriz;
+        System.out.println("Ingrese dimesion de Matriz:");
+        int dimension = lt.nextInt();
+        System.out.println("Ingrese valor inicial:");
+        int valor = lt.nextInt();
+        matriz = new String[dimension][dimension];
+        for (int f = 0; f < matriz.length; f++) {
+            if (f % 2 == 0) {
+                for (int c = matriz.length - 1; c >= 0; c--) {
+                    matriz[f][c] = String.valueOf(valor);
+                    valor = valor + 1;
+                }
+            } else {
+                for (int c = 0; c < matriz.length; c++) {
+                    matriz[f][c] = String.valueOf(valor);
+                    valor = valor + 1;
+                }
+            }
+
+        }
+        imprimirMatriz(matriz);
+    }
+
+    public void ejercicio27cch() {
+        System.out.println("Ingrese dimesion de Matriz:");
+        int dimension = lt.nextInt();
+        System.out.println("Ingrese valor inicial:");
+        int valor = lt.nextInt();
+        String[][] matriz = new String[dimension][dimension];
+        for (int f = (matriz.length - 1); f >= 0; f--) {
+            if (f % 2 == 0) {
+                for (int c = (matriz.length - 1); c >= 0; c--) {
+                    matriz[f][c] = "" + valor;
+                    valor++;
+                }
+            } else {
+                for (int c = 0; c < matriz.length; c++) {
+                    matriz[f][c] = "" + valor;
+                    valor++;
+                }
+            }
+        }
+        imprimirMatriz(matriz);
+    }
+
+    public void ejercicio28cch() {
+        System.out.println("Ingrese dimesion de Matriz:");
+        int dimension = lt.nextInt();
+        System.out.println("Ingrese valor inicial:");
+        int valor = lt.nextInt();
+        String[][] matriz = new String[dimension][dimension];
+        for (int f = (matriz.length - 1); f >= 0; f--) {
+            if (f % 2 == 0) {
+                for (int c = 0; c < matriz.length; c++) {
+                    matriz[f][c] = "" + valor;
+                    valor++;
+                }
+            } else {
+                for (int c = (matriz.length - 1); c >= 0; c--) {
+                    matriz[f][c] = "" + valor;
+                    valor++;
+                }
+            }
+        }
+        imprimirMatriz(matriz);
+    }
+
+    public void ejercicio29cch() {
+        String[][] matriz;
+        System.out.println("Ingrese dimesion de Matriz:");
+        int dimension = lt.nextInt();
+        System.out.println("Ingrese valor inicial:");
+        int valor = lt.nextInt();
+        matriz = new String[dimension][dimension];
+        for (int v = 0; v < dimension / 2; v++) {
+            for (int ls = v; ls < dimension - 1 - v; ls++) {
+                matriz[v][ls] = String.valueOf(valor);
+                valor++;
+            }
+            for (int ld = v; ld < dimension - 1 - v; ld++) {
+                matriz[ld][dimension - 1 - v] = String.valueOf(valor);
+                valor++;
+            }
+            for (int lin = dimension - 1 - v; lin > v; lin--) {
+                matriz[dimension - 1 - v][lin] = String.valueOf(valor);
+                valor++;
+            }
+            for (int liz = dimension - 1 - v; liz > v; liz--) {
+                matriz[liz][v] = String.valueOf(valor);
+                valor++;
+            }
+        }
+        if (dimension % 2 != 0) {
+            matriz[dimension / 2][dimension / 2] = String.valueOf(valor);
+        }
+        imprimirMatriz(matriz);
+    }
+
+    public void ejercicio30cch() {
+        String[][] matriz;
+        System.out.println("Ingrese dimesion de Matriz:");
+        int dimension = lt.nextInt();
+        System.out.println("Ingrese valor inicial:");
+        int valor = lt.nextInt();
+        matriz = new String[dimension][dimension];
+        for (int v = 0; v < dimension / 2; v++) {
+            for (int ld = v; ld < dimension - 1 - v; ld++) {
+                matriz[ld][dimension - 1 - v] = String.valueOf(valor);
+                valor++;
+            }
+            for (int lin = dimension - 1 - v; lin > v; lin--) {
+                matriz[dimension - 1 - v][lin] = String.valueOf(valor);
+                valor++;
+            }
+            for (int liz = dimension - 1 - v; liz > v; liz--) {
+                matriz[liz][v] = String.valueOf(valor);
+                valor++;
+            }
+            for (int ls = v; ls < dimension - 1 - v; ls++) {
+                matriz[v][ls] = String.valueOf(valor);
+                valor++;
+            }
+        }
+        if (dimension % 2 != 0) {
+            matriz[dimension / 2][dimension / 2] = String.valueOf(valor);
+        }
+        imprimirMatriz(matriz);
+    }
+
+    public void ejercicio31cch() {
+        String[][] matriz;
+        System.out.println("Ingrese dimesion de Matriz:");
+        int dimension = lt.nextInt();
+        System.out.println("Ingrese valor inicial:");
+        int valor = lt.nextInt();
+        matriz = new String[dimension][dimension];
+        for (int v = 0; v < dimension / 2; v++) {
+            for (int lin = dimension - 1 - v; lin > v; lin--) {
+                matriz[dimension - 1 - v][lin] = String.valueOf(valor);
+                valor++;
+            }
+            for (int liz = dimension - 1 - v; liz > v; liz--) {
+                matriz[liz][v] = String.valueOf(valor);
+                valor++;
+            }
+            for (int ls = v; ls < dimension - 1 - v; ls++) {
+                matriz[v][ls] = String.valueOf(valor);
+                valor++;
+            }
+            for (int ld = v; ld < dimension - 1 - v; ld++) {
+                matriz[ld][dimension - 1 - v] = String.valueOf(valor);
+                valor++;
+            }
+        }
+        if (dimension % 2 != 0) {
+            matriz[dimension / 2][dimension / 2] = String.valueOf(valor);
+        }
+        imprimirMatriz(matriz);
+    }
+
+    public void ejercicio32cch() {
+        String[][] matriz;
+        System.out.println("Ingrese dimesion de Matriz:");
+        int dimension = lt.nextInt();
+        System.out.println("Ingrese valor inicial:");
+        int valor = lt.nextInt();
+        matriz = new String[dimension][dimension];
+        for (int v = 0; v < dimension / 2; v++) {
+            for (int liz = dimension - 1 - v; liz > v; liz--) {
+                matriz[liz][v] = String.valueOf(valor);
+                valor++;
+            }
+            for (int ls = v; ls < dimension - 1 - v; ls++) {
+                matriz[v][ls] = String.valueOf(valor);
+                valor++;
+            }
+            for (int ld = v; ld < dimension - 1 - v; ld++) {
+                matriz[ld][dimension - 1 - v] = String.valueOf(valor);
+                valor++;
+            }
+            for (int lin = dimension - 1 - v; lin > v; lin--) {
+                matriz[dimension - 1 - v][lin] = String.valueOf(valor);
+                valor++;
+            }
+        }
+        if (dimension % 2 != 0) {
+            matriz[dimension / 2][dimension / 2] = String.valueOf(valor);
+        }
+        imprimirMatriz(matriz);
+    }
+
+    public void imprimirMatriz(String[][] m) {
+        for (int f = 0; f < m.length; f++) {
+            for (int c = 0; c < m[0].length; c++) {
+                if (m[f][c] == null) {
+                    System.out.print("\t");
+                } else {
+                    System.out.print(m[f][c] + "\t");
+                }
+            }
+            System.out.println("");
+        }
+    }
+
+    public static void main(String[] args) {
+        new ejerciciosCCH();
+    }
+}
